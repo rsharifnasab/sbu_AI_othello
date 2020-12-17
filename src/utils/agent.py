97e507945f708387adfcd9b528787ca20b454c11
matin <zivdar1matin@gmail.com>
2020-12-17 20:28:32 +0330

    Initialize heuristic functions

--- src/utils/agent.py
+++ src/utils/agent.py
@@ -24,7 +24,12 @@ class Ai():
     @staticmethod
     def heuristic(board, steps_passed):
         steps_passed += 1
-        return np.sum(board)
+        if steps_passed < 10:
+            return calculate_mobility(board)
+        elif steps_passed < 50:
+            return calculate_positional(board)
+        else:
+            return calculate_absolute(board)
 
     @staticmethod
     def minimax(game, depth, alpha, beta, is_computer, turn, steps_passed):
@@ -40,3 +45,46 @@ class Ai():
         for x,y in game.available_moves(turn):
             pass
 
+
+    @staticmethod
+    def calculate_positional(board):
+        """
+        based on valuable positions ( such as corners and edges)
+        """
+        mask = np.array([
+            [100, -20, 10,  5,  5, 10, -20, 100],
+            [-20, -50, -2, -2, -2, -2, -50, -20],
+            [10,  -2,  -1, -1, -1, -1,  -2, -10],
+            [5,   -2,  -1, -1, -1, -1,  -2,   5],
+            [5,   -2,  -1, -1, -1, -1,  -2,   5],
+            [10,  -2,  -1, -1, -1, -1,  -2, -10],
+            [-20, -50, -2, -2, -2, -2, -50, -20],
+            [100, -20, 10,  5,  5, 10, -20, 100],
+            ])
+        return np.multiply(board, mask)
+    
+    @staticmethod
+    def calculate_mobility(board):
+        """
+        based on number of legal moves
+        """
+        mask = np.array([
+            [2, 0, 0, 0, 0, 0, 0, 2],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 2],
+            ])
+        m_player = board.available_moves(1)
+        m_opponent = board.available_moves(-1)
+        return 10*np.multiply(board, mask) + (m_player-m_opponent)/(m_player+m_opponent)
+
+    @staticmethod
+    def calculate_absolute(board):
+        """
+        based on number of pieces
+        """
+        return np.sum(board)

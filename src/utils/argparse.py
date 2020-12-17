31d58836ca23d92a34ed64d5ba089aeab1b18b23
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2020-12-17 14:53:27 +0330

    moved arg parse out of function

--- src/utils/argparse.py
+++ src/utils/argparse.py
@@ -1,18 +1,20 @@
 from argparse import ArgumentParser
 from .ui import Cli, QT
 
+parser = ArgumentParser(
+    prog="src/play.py",
+    description="awesome othello AI",
+    epilog="enjoy playing :D"
+)
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-c", "--cli", action="store_true",
+                   help="play game in command mode")
+group.add_argument("-g", "--gui", action="store_true",
+                   help="play game in GUI mode")
+
+
 
 def ui_chooser():
-    parser = ArgumentParser(
-        prog="src/play.py",
-        description="awesome othello AI",
-        epilog="enjoy playing :D"
-    )
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-c", "--cli", action="store_true",
-                       help="play game in command mode")
-    group.add_argument("-g", "--gui", action="store_true",
-                       help="play game in GUI mode")
     if parser.parse_args().cli:
         return Cli()
     else:
